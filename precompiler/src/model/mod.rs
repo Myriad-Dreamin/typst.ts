43bb41075f3d945bedfc5494c5804e8a38ddc33b
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-09 12:14:07 +0800

    dev(precompiler): introduce browser world

--- precompiler/src/model/mod.rs
+++ precompiler/src/model/mod.rs
@@ -2,3 +2,10 @@ pub(crate) mod system_world;
 pub use system_world::SystemFontSearcher;
 pub use system_world::TypstSystemWorld;
 
+// todo: make compiler work in browser
+// #[cfg(feature = "web")]
+// pub(crate) mod browser_world;
+// #[cfg(feature = "web")]
+// pub use browser_world::BrowserFontSearcher;
+// #[cfg(feature = "web")]
+// pub use browser_world::TypstBrowserWorld;
