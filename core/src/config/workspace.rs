ec1358d5b0080a265c2e3de03a30f4afe4756917
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-12 10:45:47 +0800

    dev(core): add workspace config

---
+++ core/src/config/workspace.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
+pub struct Config {
+    pub version: String,
+
+    /// Path to typst workspace.
+    pub workspace: String,
+
+    /// Path to entries
+    pub files: Vec<String>,
+
+    #[serde(rename = "fontPaths")]
+    pub font_paths: Vec<String>,
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,7 @@
 pub(crate) mod artifact;
 pub use artifact::Artifact;
 
+pub mod config;
+
 pub(crate) mod font;
 pub use font::FontResolver;
