9671b362dcffe7bd1a3a2470479425787b535b83
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-16 11:18:46 +0800

    dev(renderer): add private renderXXXLayer + renderDisplayLayer + renderTextLayer

--- renderer/src/driver/main.ts
+++ renderer/src/driver/main.ts
@@ -71,71 +71,77 @@ class TypstRendererImpl {
         return this.renderer.render_to_pdf(artifact_content);
     }
 
-    async render(artifact_content: string, imageContainer: HTMLDivElement): Promise<RenderResult> {
+    private async renderDisplayLayer(artifact_content: string, imageContainer: HTMLDivElement): Promise<ImageData>  {
         let canvas = document.createElement('canvas');
 
         const imageContainerWidth = imageContainer.offsetWidth;
+    
+        const t = performance.now();
+        const imageRenderResult = await this.renderImage(artifact_content);
+        const t2 = performance.now();
+
+        canvas.width = imageRenderResult.width;
+        canvas.height = imageRenderResult.height;
+        let ctx = canvas.getContext('2d');
+        if (ctx) {
+            ctx.putImageData(imageRenderResult, 0, 0);
+        }
+
+        const t3 = performance.now();
+        console.log("time used", t2-t, t3-t2);
+
+        // compute scaling factor according to the paper size
+        const currentScale = imageContainerWidth / imageRenderResult.width;
+        imageContainer.style.transformOrigin = "0px 0px";
+        imageContainer.style.transform = `scale(${currentScale})`;
+
+        imageContainer.appendChild(canvas);
+
+        return imageRenderResult;
+    }
+
+    private async renderTextLayer(artifact_content: string, imageContainer: HTMLDivElement)  {
+        const imageContainerWidth = imageContainer.offsetWidth;
+
+        const t2 = performance.now();
+        const layer = document.getElementById("text-layer");
+        const data = await this.renderPdf(artifact_content)
+        const pdfDoc = await this.pdf.getDocument(data).promise;
+        const t3 = performance.now();
+
+        const page = await pdfDoc.getPage(1);
+        const textLayerScale = Number.parseFloat((imageContainerWidth / page.getViewport({ scale: 1 }).width).toFixed(4));
+        layer?.parentElement?.style.setProperty('--scale-factor', textLayerScale.toString());
+
+        page.getTextContent().then((textContent) => {
+            console.log(textContent);
+
+            this.pdf.renderTextLayer({
+                textContentSource: textContent,
+                container: layer!,
+                viewport: page.getViewport( { scale: textLayerScale } ),
+            });
+            const t4 = performance.now();
+
+            console.log("text layer used", t3-t2, t4-t3);
+        });
+    }
+
+    async render(artifact_content: string, imageContainer: HTMLDivElement): Promise<RenderResult> {
         let renderResult: RenderResult;
 
-        const renderDisplayLayer = async () => {
-    
-            const t = performance.now();
-            const imageRenderResult = await this.renderImage(artifact_content);
-            const t2 = performance.now();
-    
-            canvas.width = imageRenderResult.width;
-            canvas.height = imageRenderResult.height;
-            let ctx = canvas.getContext('2d');
-            if (ctx) {
-                ctx.putImageData(imageRenderResult, 0, 0);
-            }
-    
-            const t3 = performance.now();
-            console.log("time used", t2-t, t3-t2);
-    
-            // compute scaling factor according to the paper size
-            const currentScale = imageContainerWidth / imageRenderResult.width;
-            imageContainer.style.transformOrigin = "0px 0px";
-            imageContainer.style.transform = `scale(${currentScale})`;
-    
-            imageContainer.appendChild(canvas);
-
-            renderResult = imageRenderResult;
+        const doRenderDisplayLayer = async () => {
+            renderResult = await this.renderDisplayLayer(artifact_content, imageContainer);
         };
 
-        const renderTextLayer = new Promise((resolve) => {
+        const doRenderTextLayer = new Promise((resolve) => {
             setTimeout(() => { // setImmediate
-                (async () => {
-                    const t2 = performance.now();
-                    const layer = document.getElementById("text-layer");
-                    const data = await this.renderPdf(artifact_content)
-                    const pdfDoc = await this.pdf.getDocument(data).promise;
-                    const t3 = performance.now();
-        
-                    const page = await pdfDoc.getPage(1);
-                    const textLayerScale = Number.parseFloat((imageContainerWidth / page.getViewport({ scale: 1 }).width).toFixed(4));
-                    layer?.parentElement?.style.setProperty('--scale-factor', textLayerScale.toString());
-        
-                    page.getTextContent().then((textContent) => {
-                        console.log(textContent);
-            
-                        this.pdf.renderTextLayer({
-                            textContentSource: textContent,
-                            container: layer!,
-                            viewport: page.getViewport( { scale: textLayerScale } ),
-                        });
-                        const t4 = performance.now();
-        
-                        console.log("text layer used", t3-t2, t4-t3);
-                    });
-    
-                    resolve(undefined);
-                })();
+                this.renderTextLayer(artifact_content, imageContainer).then(resolve);
             }, 0);
         })
 
         return Promise.all([
-            renderDisplayLayer(), renderTextLayer,
+            doRenderDisplayLayer(), doRenderTextLayer,
         ]).then(() => {
             return {
                 width: renderResult.width,
