6b483ec9085dde93b3ad2f4ed34f373246f93fc6
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-23 19:28:30 +0800

    dev(pkg::core): render multiple pages

--- packages/typst.ts/src/driver/driver.ts
+++ packages/typst.ts/src/driver/driver.ts
@@ -6,22 +6,50 @@ import typstInit, * as typst from '../../pkg/typst_renderer_ts';
 import type * as pdfjsModule from 'pdfjs-dist';
 import type { InitOptions, BeforeBuildMark } from './options.init';
 
-export interface RenderOptions {
-  artifactContent: string;
+export class RenderPageOptions {
+  page_off: number;
+}
+
+export interface RenderOptionsBase {
   container: HTMLDivElement;
 
   backgroundColor?: string;
   pixelPerPt?: number;
 }
 
+export interface RenderByStringOptions extends RenderOptionsBase {
+  artifactContent: string;
+}
+
+export interface RenderInSessionOptions extends RenderOptionsBase {
+  renderSession: RenderSession;
+}
+
+export type RenderOptions = RenderByStringOptions | RenderInSessionOptions;
+
 export interface RenderResult {
   width: number;
   height: number;
 }
 
+export interface RenderSession {
+  readonly background_color: string;
+  readonly pixel_per_pt: number;
+}
+
 export interface TypstRenderer {
   init(options?: Partial<InitOptions>): Promise<void>;
   render(options: RenderOptions): Promise<RenderResult>;
+
+  /// run a function with a session, and the sesssion is only available during the
+  /// function call.
+  ///
+  /// the lifetime of session is quite bug-prone, so we current does not make it
+  /// longer live than the function call.
+  runWithSession<T>(
+    options: RenderByStringOptions,
+    fn: (session: RenderSession) => Promise<T>,
+  ): Promise<T>;
 }
 
 const once = <T>(fn: () => T) => {
@@ -68,11 +96,7 @@ class TypstRendererDriver {
     this.renderer = await builder.build();
   }
 
-  async renderImage(artifactContent: string, options?: RenderOptions): Promise<ImageData> {
-    if (!options) {
-      return this.renderer.render(artifactContent);
-    }
-
+  private imageOptionsToRust(options: RenderOptions): typst.RenderImageOptions {
     const rustOptions = new typst.RenderImageOptions();
     rustOptions.pixel_per_pt = options.pixelPerPt ?? 2;
 
@@ -87,59 +111,101 @@ class TypstRendererDriver {
       rustOptions.background_color = options.backgroundColor.slice(1);
     }
 
-    return this.renderer.render(artifactContent, /* moved */ rustOptions);
+    return rustOptions;
+  }
+
+  async renderImage(artifactContent: string, options?: RenderOptions): Promise<ImageData> {
+    if (!options) {
+      return this.renderer.render(artifactContent);
+    }
+
+    return this.renderer.render(artifactContent, /* moved */ this.imageOptionsToRust(options));
+  }
+
+  async renderImageInSession(
+    session: RenderSession,
+    options?: RenderPageOptions,
+  ): Promise<ImageData> {
+    if (!options) {
+      return this.renderer.render_page(session as typst.RenderSession);
+    }
+
+    const rustOptions = new typst.RenderPageImageOptions();
+    rustOptions.page_off = options.page_off;
+
+    return this.renderer.render_page(session as typst.RenderSession, rustOptions);
   }
 
   async renderPdf(artifactContent: string): Promise<Uint8Array> {
     return this.renderer.render_to_pdf(artifactContent);
   }
 
+  async renderPdfInSession(session: RenderSession): Promise<Uint8Array> {
+    return this.renderer.render_to_pdf_in_session(session as typst.RenderSession);
+  }
+
   private async renderDisplayLayer(
-    artifactContent: string,
+    session: typst.RenderSession,
     container: HTMLDivElement,
+    canvasList: HTMLCanvasElement[],
     options: RenderOptions,
-  ): Promise<ImageData> {
-    const containerWidth = container.offsetWidth;
-    const imageScaleFactor = options.pixelPerPt ?? 2;
+  ): Promise<RenderResult> {
+    const pages_info = session.pages_info;
+    const page_count = pages_info.page_count;
 
     const t = performance.now();
 
-    const artifact = JSON.parse(artifactContent);
+    /// render each page
+    const renderResult = (
+      await Promise.all(
+        //   canvasList.map(async (canvas, i) => {
+        //     const renderResult = await this.renderImageInSession(session, {
+        //       page_off: i,
+        //     });
+        //     console.log(cyrb53(renderResult.data));
+        //     let ctx = canvas.getContext('2d');
+        //     if (ctx) {
+        //       ctx.putImageData(renderResult, 0, 0);
+        //     }
 
-    const canvasList = new Array(artifact.pages.length);
-    for (let i = 0; i < artifact.pages.length; i++) {
-      const canvas = (canvasList[i] = document.createElement('canvas'));
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        const pageAst = artifact.pages[i];
-        const width = Math.ceil(pageAst.size.x) * imageScaleFactor;
-        const height = Math.ceil(pageAst.size.y) * imageScaleFactor;
+        //     return {
+        //       width: renderResult.width,
+        //       height: renderResult.height,
+        //     };
+        //   }),
+        // )
 
-        canvas.width = width;
-        canvas.height = height;
-      }
+        /// seq
+        [
+          (async () => {
+            let i0RenderResult: RenderResult = undefined as unknown as RenderResult;
+            for (let i = 0; i < page_count; i++) {
+              const canvas = canvasList[i];
+              const renderResult = await this.renderImageInSession(session, {
+                page_off: i,
+              });
+              let ctx = canvas.getContext('2d');
+              if (ctx) {
+                ctx.putImageData(renderResult, 0, 0);
+              }
 
-      container.appendChild(canvas);
-    }
+              if (i === 0) {
+                i0RenderResult = renderResult;
+              }
+            }
 
-    const t2 = performance.now();
-
-    const renderResult = await this.renderImage(artifactContent, options);
-    let ctx = canvasList[0].getContext('2d');
-    if (ctx) {
-      ctx.putImageData(renderResult, 0, 0);
-    }
+            return {
+              width: i0RenderResult.width,
+              height: i0RenderResult.height,
+            };
+          })(),
+        ],
+      )
+    )[0]!;
 
     const t3 = performance.now();
 
-    console.log(
-      `display layer used: retieve/render = ${(t2 - t).toFixed(1)}/${(t3 - t2).toFixed(1)}ms`,
-    );
-
-    // compute scaling factor according to the paper size
-    const currentScale = containerWidth / renderResult.width;
-    container.style.transformOrigin = '0px 0px';
-    container.style.transform = `scale(${currentScale})`;
+    console.log(`display layer used: render = ${(t3 - t).toFixed(1)}ms`);
 
     return renderResult;
   }
@@ -150,6 +216,7 @@ class TypstRendererDriver {
     scale: number,
   ) {
     const textContentSource = await page.getTextContent();
+    console.log({ scale });
     this.pdf.renderTextLayer({
       textContentSource,
       container,
@@ -157,24 +224,35 @@ class TypstRendererDriver {
     });
   }
 
-  private async renderTextLayer(artifact_content: string, container: HTMLDivElement) {
-    const layer = document.getElementById('text-layer')!;
+  private async renderTextLayer(
+    session: typst.RenderSession,
+    container: HTMLDivElement,
+    layerList: HTMLDivElement[],
+  ) {
     const containerWidth = container.offsetWidth;
     const t2 = performance.now();
 
-    const buf = await this.renderPdf(artifact_content);
+    const buf = await this.renderPdfInSession(session);
     const doc = await this.pdf.getDocument(buf).promise;
     const t3 = performance.now();
 
-    const page = await doc.getPage(1);
+    const pages_info = session.pages_info;
+    const page_count = pages_info.page_count;
 
-    // compute scale size
-    const orignalScale = containerWidth / page.getViewport({ scale: 1 }).width;
-    // the --scale-factor will truncate our scale, we do it first
-    const scale = Number.parseFloat(orignalScale.toFixed(4));
-    layer.parentElement?.style.setProperty('--scale-factor', scale.toString());
+    if (page_count === 0) {
+      throw new Error(`No page found in session ${session}`);
+    }
 
-    this.renderOnePageTextLayer(layer, page, scale);
+    const renderOne = async (layer: HTMLDivElement, i: number) => {
+      const page = await doc.getPage(i + 1);
+      const orignalScale = containerWidth / page.getViewport({ scale: 1 }).width;
+      // the --scale-factor will truncate our scale, we do it first
+      const scale = Number.parseFloat(orignalScale.toFixed(4));
+      layer.parentElement?.style.setProperty('--scale-factor', scale.toString());
+      this.renderOnePageTextLayer(layer, page, scale);
+    };
+
+    await Promise.all(layerList.map(renderOne));
     const t4 = performance.now();
 
     console.log(
@@ -183,26 +261,162 @@ class TypstRendererDriver {
   }
 
   async render(options: RenderOptions): Promise<RenderResult> {
-    const { artifactContent, container } = options;
+    let session: typst.RenderSession;
     let renderResult: RenderResult;
 
-    const doRenderDisplayLayer = async () => {
-      renderResult = await this.renderDisplayLayer(artifactContent, container, options);
+    const doRenderDisplayLayer = async (canvasList: HTMLCanvasElement[]) => {
+      renderResult = await this.renderDisplayLayer(session, options.container, canvasList, options);
     };
 
-    const doRenderTextLayer = new Promise(resolve => {
-      setTimeout(() => {
-        // setImmediate
-        this.renderTextLayer(artifactContent, container).then(resolve);
-      }, 0);
-    });
+    const doRenderTextLayer = (layerList: HTMLDivElement[]) =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          // setImmediate
+          this.renderTextLayer(session, options.container, layerList).then(resolve);
+        }, 0);
+      });
 
-    return Promise.all([doRenderDisplayLayer(), doRenderTextLayer]).then(() => {
-      return {
-        width: renderResult.width,
-        height: renderResult.height,
-      };
+    return this.withinOptionSession(options, async sessionRef => {
+      session = sessionRef;
+
+      const container = options.container;
+
+      const imageScaleFactor = options.pixelPerPt ?? 2;
+
+      const pages_info = session.pages_info;
+      const page_count = pages_info.page_count;
+
+      if (page_count === 0) {
+        throw new Error(`No page found in session ${session}`);
+      }
+
+      const t = performance.now();
+
+      container.innerHTML = '';
+      container.style.width = '100%';
+
+      /// create canvas for each page
+      const canvasList = new Array(page_count);
+      const layerList = new Array(page_count);
+      const commonList = new Array(page_count);
+      const textLayerParentList = new Array(page_count);
+      for (let i = 0; i < page_count; i++) {
+        const pageAst = pages_info.page(i);
+        const width = Math.ceil(pageAst.width_pt) * imageScaleFactor;
+        const height = Math.ceil(pageAst.height_pt) * imageScaleFactor;
+
+        // const commonDiv = document.createElement('div');
+        const commonDiv = (commonList[i] = document.createElement('div'));
+        const canvas = (canvasList[i] = document.createElement('canvas'));
+        const textLayer = (layerList[i] = document.createElement('div'));
+        const textLayerParent = (textLayerParentList[i] = document.createElement('div'));
+
+        const ctx = canvas.getContext('2d');
+        if (ctx) {
+          const canvasDiv = document.createElement('div');
+
+          canvas.width = width;
+          canvas.height = height;
+
+          canvasDiv.appendChild(canvas);
+          // canvasDiv.style.zIndex = '-1';
+          // canvas.style.zIndex = '-1';
+          commonDiv.appendChild(canvasDiv);
+          canvasDiv.style.position = 'absolute';
+        }
+
+        {
+          textLayerParent.appendChild(textLayer);
+
+          textLayerParent.className = 'text-layer textLayer';
+
+          /// on width change
+          console.log('resize', i, container.offsetWidth);
+          const containerWidth = container.offsetWidth;
+          const orignalScale = containerWidth / width;
+          textLayerParent.style.width = `${containerWidth}px`;
+          textLayerParent.style.height = `${height * orignalScale}px`;
+          commonDiv.style.width = `${containerWidth}px`;
+          commonDiv.style.height = `${height * orignalScale}px`;
+
+          // textLayerParent.style.zIndex = '1';
+          commonDiv.appendChild(textLayerParent);
+          textLayerParent.style.position = 'absolute';
+        }
+
+        commonDiv.style.position = 'relative';
+        container.appendChild(commonDiv);
+        // const clearDiv = document.createElement('div');
+        // clearDiv.style.zIndex = '-1';
+        // container.appendChild(clearDiv);
+      }
+
+      const t2 = performance.now();
+
+      console.log(`layer used: retieve = ${(t2 - t).toFixed(1)}ms`);
+
+      await Promise.all([doRenderDisplayLayer(canvasList), doRenderTextLayer(layerList)]);
+
+      /// resize again to avoid bad width change after render
+      for (let i = 0; i < page_count; i++) {
+        const pageAst = pages_info.page(i);
+        const width = Math.ceil(pageAst.width_pt) * imageScaleFactor;
+        const height = Math.ceil(pageAst.height_pt) * imageScaleFactor;
+
+        const canvasDiv = canvasList[i].parentElement!;
+        const commonDiv = commonList[i];
+        const textLayerParent = textLayerParentList[i];
+
+        /// on width change
+        console.log('resize', i, container.offsetWidth);
+        const containerWidth = container.offsetWidth;
+        const orignalScale = containerWidth / width;
+        textLayerParent.style.width = `${containerWidth}px`;
+        textLayerParent.style.height = `${height * orignalScale}px`;
+        commonDiv.style.width = `${containerWidth}px`;
+        commonDiv.style.height = `${height * orignalScale}px`;
+
+        // compute scaling factor according to the paper size
+        const currentScale = container.offsetWidth / width;
+        canvasDiv.style.transformOrigin = '0px 0px';
+        canvasDiv.style.transform = `scale(${currentScale})`;
+      }
+      return renderResult;
     });
+  }
+
+  private withinOptionSession<T>(
+    options: RenderOptions,
+    fn: (session: typst.RenderSession) => Promise<T>,
+  ): Promise<T> {
+    if ('renderSession' in options) {
+      return fn(options.renderSession as typst.RenderSession);
+    }
+
+    if ('artifactContent' in options) {
+      return this.runWithSession(options, fn);
+    }
+
+    throw new Error(
+      'Invalid render options, should be one of RenderByStringOptions|RenderBySessionOptions',
+    );
+  }
+
+  async runWithSession<T>(
+    options: RenderByStringOptions,
+    fn: (session: typst.RenderSession) => Promise<T>,
+  ): Promise<T> {
+    const session = this.renderer.create_session(
+      options.artifactContent,
+      /* moved */ this.imageOptionsToRust(options),
+    );
+    try {
+      console.log(`session`, JSON.stringify(session), `activated`);
+      return await fn(session);
+    } finally {
+      console.log(`session`, JSON.stringify(session), `deactivated`);
+      session.free();
+    }
   }
 }
 
