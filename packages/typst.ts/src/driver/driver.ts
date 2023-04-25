e399fe95a0d6c56a8ad2a46e79ee6d0e77af0660
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-26 02:00:24 +0800

    dev(pkg::core): add comments

--- packages/typst.ts/src/driver/driver.ts
+++ packages/typst.ts/src/driver/driver.ts
@@ -6,10 +6,22 @@ import typstInit, * as typst from '../../pkg/typst_renderer_ts';
 import type * as pdfjsModule from 'pdfjs-dist';
 import type { InitOptions, BeforeBuildMark } from './options.init';
 
+/**
+ * The options for rendering a page to an image.
+ * @typedef {Object} RenderPageOptions
+ * @property {number} page_off - The page offset to render.
+ */
 export class RenderPageOptions {
   page_off: number;
 }
 
+/**
+ * The options for rendering a Typst document to specified container.
+ * @typedef {Object} RenderOptionsBase
+ * @property {HTMLDivElement} container - The container to render the Typst document.
+ * @property {string} [backgroundColor] - The background color will replace the default one by typst document.
+ * @property {number} [pixelPerPt] - The pixel per point scale up the image, which is 2.5 by default and recommended.
+ */
 export interface RenderOptionsBase {
   container: HTMLDivElement;
 
@@ -17,30 +29,65 @@ export interface RenderOptionsBase {
   pixelPerPt?: number;
 }
 
+/**
+ * The options for rendering a Typst document to specified container.
+ * @typedef {Object} RenderByStringOptions
+ * @property {string} artifactContent - The Typst document content.
+ */
 export interface RenderByStringOptions extends RenderOptionsBase {
   artifactContent: string;
 }
 
+/**
+ * The options for rendering a Typst document to specified container.
+ * @typedef {Object} RenderInSessionOptions
+ * @property {RenderSession} renderSession - The Typst document session that has been created by TypstRenderer.
+ */
 export interface RenderInSessionOptions extends RenderOptionsBase {
   renderSession: RenderSession;
 }
 
+/**
+ * The options for rendering a Typst document to specified container.
+ * @typedef {Object} RenderByStringOptions
+ * @description see {@link RenderByStringOptions} and {@link RenderInSessionOptions}
+ */
 export type RenderOptions = RenderByStringOptions | RenderInSessionOptions;
 
+/**
+ * The result of rendering a Typst document.
+ * @typedef {Object} RenderResult
+ * @property {number} width - The width of the rendered Typst document (single page).
+ * @property {number} height - The height of the rendered Typst document (single page).
+ */
 export interface RenderResult {
   width: number;
   height: number;
 }
 
+/**
+ * The session of a Typst document.
+ * @typedef {Object} RenderSession
+ * @property {string} background_color - The background color of the Typst document.
+ * @property {number} pixel_per_pt - The pixel per point scale up the image.
+ *
+ * caution: the underlying object is created by the wasm module, which means that
+ *   + any modification will raise an error.
+ *   + Never clone the object and pass it back to typst renderer.
+ *   + You must not hold a reference, since it will be freed after a while
+ */
 export interface RenderSession {
   readonly background_color: string;
   readonly pixel_per_pt: number;
 }
-
-interface RenderFeature {
-  render(artifactContext: string, options?: typst.RenderSessionOptions): Promise<ImageData>;
-}
-
+/**
+ * The interface of Typst renderer.
+ * @typedef {Object} TypstRenderer
+ * @property {function} init - Initialize the Typst renderer.
+ * @property {function} render - Render a Typst document to specified container.
+ * @property {function} runWithSession - Run a function with a session to interact
+ *   with the wasm module multiple times programmatically.
+ */
 export interface TypstRenderer {
   init(options?: Partial<InitOptions>): Promise<void>;
   render(options: RenderOptions): Promise<RenderResult>;
@@ -54,6 +101,26 @@ export interface TypstRenderer {
     options: RenderByStringOptions,
     fn: (session: RenderSession) => Promise<T>,
   ): Promise<T>;
+}
+
+/**
+ * create a Typst renderer.
+ * @param {typeof pdfjsModule} pdf - The pdfjs module.
+ * @returns {TypstRenderer} - The Typst renderer.
+ * @example
+ * ```typescript
+ * import { createTypstRenderer } from 'typst';
+ * import * as pdfjs from 'pdfjs-dist';
+ * const renderer = createTypstRenderer(pdfjs);
+ * await renderer.init();
+ * await renderer.render({
+ *   container: document.getElementById('container'),
+ *   artifactContent: '{ ... }',
+ * });
+ * ```
+ */
+export function createTypstRenderer(pdf: typeof pdfjsModule): TypstRenderer {
+  return new TypstRendererDriver(pdf);
 }
 
 const once = <T>(fn: () => T) => {
@@ -438,8 +505,4 @@ class TypstRendererDriver {
       throw e;
     }
   }
-}
-
-export function createTypstRenderer(pdf: typeof pdfjsModule): TypstRenderer {
-  return new TypstRendererDriver(pdf);
 }
--- packages/typst.ts/src/driver/options.init.ts
+++ packages/typst.ts/src/driver/options.init.ts
@@ -1,24 +1,75 @@
 // @ts-ignore
 import type * as typst from '../../pkg/typst_renderer_ts';
 
+/**
+ * staged options function
+ * @typedef StagedOptFn
+ * @template S - stage mark
+ * @template T - context type
+ */
 export type StagedOptFn<S extends symbol, T = any> = (s: S, t: T) => Promise<void>;
 
-/// this mark is used to identify the beforeBuild stage
+/**
+ * this mark is used to identify the beforeBuild stage
+ * @type {unique symbol}
+ * @description will not be used in runtime code
+ */
 const BeforeBuildSymbol = Symbol('beforeBuild');
+
+/**
+ * this mark is used to identify the beforeBuild stage
+ * @typedef BeforeBuildMark
+ * @description cannot be created by any runtime code
+ */
 export type BeforeBuildMark = typeof BeforeBuildSymbol;
+
+/**
+ * before build stage
+ * @typedef BeforeBuildFn
+ * @description possible created by:
+ *   - preloadRemoteFonts
+ *   - preloadSystemFonts
+ */
 export type BeforeBuildFn = StagedOptFn<BeforeBuildMark, any>;
 
+/**
+ *
+ * @typedef InitOptions
+ * @property {BeforeBuildFn[]} beforeBuild - callbacks before build stage
+ */
 export interface InitOptions {
-  /// before build stage, the registered functions will be executed in order
-  /// possible options:
-  /// - preloadRemoteFonts
-  /// - preloadSystemFonts
+  /**
+   * callbacks before build stage
+   *
+   * before build stage, the registered functions will be executed in order
+   * possible options:
+   * - preloadRemoteFonts
+   * - preloadSystemFonts
+   */
   beforeBuild: BeforeBuildFn[];
 }
 
 /// preload remote fonts
 ///
 /// @param fonts - url path to font files
+
+/**
+ * preload remote fonts
+ *
+ * @param fonts - url path to font files
+ * @returns {BeforeBuildFn}
+ * @example
+ * ```typescript
+ * import { init, preloadRemoteFonts } from 'typst';
+ * init({
+ *   beforeBuild: [
+ *     preloadRemoteFonts([
+ *      'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2', // remote url
+ *      'dist/fonts/Roboto-Regular.ttf', // relative to the root of the website
+ *     ]),
+ *   ],
+ * });
+ */
 export function preloadRemoteFonts(fonts: string[]): BeforeBuildFn {
   return async (_, { ref, builder }: InitContext) => {
     await Promise.all(fonts.map(font => ref.loadFont(builder, font)));
@@ -28,6 +79,23 @@ export function preloadRemoteFonts(fonts: string[]): BeforeBuildFn {
 /// preload system fonts
 ///
 /// @param byFamily - filter system fonts to preload by family name
+
+/**
+ * preload system fonts
+ * @param byFamily - filter system fonts to preload by family name
+ * @returns {BeforeBuildFn}
+ * @example
+ * ```typescript
+ * import { init, preloadSystemFonts } from 'typst';
+ * init({
+ *   beforeBuild: [
+ *     preloadSystemFonts({
+ *       byFamily: ['Roboto'], // preload fonts by family name
+ *     }),
+ *   ],
+ * });
+ * ```
+ */
 export function preloadSystemFonts({ byFamily }: { byFamily?: string[] }): BeforeBuildFn {
   return async (_, { builder }: InitContext) => {
     const t = performance.now();
