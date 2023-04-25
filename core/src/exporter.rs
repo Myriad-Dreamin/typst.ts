f075c6146907bfc11ca844eaf49c8bb81d465419
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-26 01:19:13 +0800

    dev(core): add ArtifactExporter and comments

--- core/src/exporter.rs
+++ core/src/exporter.rs
@@ -1,5 +1,12 @@
 use typst::{diag::SourceResult, World};
 
 pub trait DocExporter {
+    /// Export the given document with given world.
+    /// the writable world is hiden by trait itself.
     fn export(&self, world: &dyn World, output: &typst::doc::Document) -> SourceResult<()>;
+}
+
+pub trait ArtifactExporter {
+    /// Export the given artifact with given world.
+    fn export(&self, world: &dyn World, output: &crate::Artifact) -> SourceResult<()>;
 }
