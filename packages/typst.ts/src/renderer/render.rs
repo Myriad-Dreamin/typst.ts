60cae514a615f1b25a32b36a6ee61213b23fe72a
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-26 07:32:31 +0800

    dev(pkg::core): test with showing canvas

--- packages/typst.ts/src/renderer/render.rs
+++ packages/typst.ts/src/renderer/render.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
-#[cfg(target_arch = "wasm32")]
+// #[cfg(target_arch = "wasm32")]
 mod tests {
     use wasm_bindgen::JsCast;
     use wasm_bindgen_test::*;
     wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
     #[wasm_bindgen_test]
-    fn artifact_deserialization() {
+    fn render_test() {
         let artifact = include_bytes!("../../main.artifact.json");
         let artifact = String::from_utf8_lossy(artifact);
 
@@ -17,7 +17,9 @@ mod tests {
 
         let mut renderer = crate::tests::get_renderer();
 
-        let session = renderer.create_session(artifact.to_string(), None).unwrap();
+        let mut session = renderer.create_session(artifact.to_string(), None).unwrap();
+        session.background_color = "343541".to_string();
+        session.pixel_per_pt = 1.0;
 
         let canvas = window
             .document()
@@ -26,6 +28,9 @@ mod tests {
             .unwrap()
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .unwrap();
+        let sizes = session.doc.pages[0].size();
+        canvas.set_width(sizes.x.to_pt() as u32);
+        canvas.set_height(sizes.y.to_pt() as u32);
 
         let context = canvas
             .get_context("2d")
@@ -43,6 +48,14 @@ mod tests {
 
             (end - start, artifact)
         };
+
+        window
+            .document()
+            .unwrap()
+            .body()
+            .unwrap()
+            .append_child(&canvas)
+            .unwrap();
 
         console_log!("canvas {}ms", serde_task.0);
     }
