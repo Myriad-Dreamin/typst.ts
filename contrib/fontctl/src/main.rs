8314aabc22bdab3437811bfa9bb24f34491cbf2f
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-27 14:46:37 +0800

    dev: bump version to 0.1.6

--- contrib/fontctl/src/main.rs
+++ contrib/fontctl/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-#[clap(name = "typst-ts-fontctl", version = "0.1.5")]
+#[clap(name = "typst-ts-fontctl", version = "0.1.6")]
 pub struct Opts {
     /// Use font assets in branch of typst
     #[clap(long, default_value = "")]
