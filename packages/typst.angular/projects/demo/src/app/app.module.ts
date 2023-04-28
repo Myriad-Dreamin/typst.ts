e3ccf11e1b886d1ebdead0069ea778c8e15736e6
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-28 09:08:30 +0800

    dev(pkg::angular): demo test import

--- packages/typst.angular/projects/demo/src/app/app.module.ts
+++ packages/typst.angular/projects/demo/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TypstDocumentModule } from '@myriaddreamin/typst.angular';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TypstDocumentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
