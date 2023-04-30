acf7d5a65a10f3a662846d6f645db6bc45160867
Myriad-Dreamin <camiyoru@gmail.com>
2023-04-30 13:28:20 +0800

    dev(pkg::angular): implement document

--- packages/typst.angular/projects/typst.angular/src/lib/typst.document.component.ts
+++ packages/typst.angular/projects/typst.angular/src/lib/typst.document.component.ts
@@ -1,8 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { withGlobalRenderer } from '@myriaddreamin/typst.ts/dist/contrib/global-renderer';
+import * as typst from '@myriaddreamin/typst.ts';
 
 @Component({
   selector: 'typst-document',
-  template: ` <p>typst.document works!</p> `,
+  templateUrl: `./typst.document.component.html`,
   styles: [],
 })
-export class TypstDocumentComponent {}
+export class TypstDocumentComponent {
+  _artifact: string = '';
+  @ViewChild('typst_app') typst_app?: ElementRef<HTMLDivElement>;
+
+  @Input() fill: string = '#ffffff';
+
+  @Input()
+  set artifact(artifact: string) {
+    this._artifact = artifact;
+    this.applyArtifact();
+  }
+
+  get artifact(): string {
+    return this._artifact;
+  }
+
+  constructor() {}
+
+  applyArtifact() {
+    if (this.typst_app?.nativeElement) {
+      const displayDiv = this.typst_app?.nativeElement;
+      if (this.artifact) {
+        const doRender = (renderer: typst.TypstRenderer) => {
+          console.log(renderer);
+          return renderer.render({
+            artifactContent: this.artifact,
+            backgroundColor: this.fill,
+            container: displayDiv,
+            pixelPerPt: 8,
+          });
+        };
+
+        /// render after init
+        withGlobalRenderer(
+          (window as unknown as any).pdfjsLib,
+          {
+            beforeBuild: [
+              typst.preloadRemoteFonts([
+                'http://localhost:20811/fonts/LinLibertine_R.ttf',
+                'http://localhost:20811/fonts/LinLibertine_RB.ttf',
+                'http://localhost:20811/fonts/LinLibertine_RBI.ttf',
+                'http://localhost:20811/fonts/LinLibertine_RI.ttf',
+                'http://localhost:20811/fonts/NewCMMath-Book.otf',
+                'http://localhost:20811/fonts/NewCMMath-Regular.otf',
+              ]),
+              typst.preloadSystemFonts({
+                byFamily: ['Segoe UI Symbol'],
+              }),
+            ],
+          },
+          doRender,
+        );
+      } else {
+        displayDiv.innerHTML = '';
+      }
+    }
+  }
+}
