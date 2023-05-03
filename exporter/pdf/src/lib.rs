bc68d2a7c2ffcaa2ef985f738a266be410b26022
Myriad-Dreamin <camiyoru@gmail.com>
2023-05-03 11:44:17 +0800

    dev(core): lift exporter utils

--- exporter/pdf/src/lib.rs
+++ exporter/pdf/src/lib.rs
@@ -1,28 +1,9 @@
 pub use typst::export::pdf;
 use typst_ts_core::DocumentExporter;
 
-use std::error::Error;
-
 use typst::{diag::SourceResult, World};
+pub(crate) use typst_ts_core::exporter_utils::*;
 
-/// Convert the given error to a vector of source errors.
-fn map_err<E: Error>(world: &dyn World, e: E) -> Box<Vec<typst::diag::SourceError>> {
-    Box::new(vec![typst::diag::SourceError::new(
-        typst::syntax::Span::new(world.main().id(), 0),
-        e.to_string(),
-    )])
-}
-
-/// export document to file system
-fn write_to_path<C: AsRef<[u8]>>(
-    world: &dyn World,
-    path: Option<std::path::PathBuf>,
-    content: C,
-) -> SourceResult<()> {
-    path.map_or(Ok(()), |path| {
-        std::fs::write(path, content).map_err(|e| map_err(world, e))
-    })
-}
 pub struct PdfDocExporter {
     path: Option<std::path::PathBuf>,
 }
--- exporter/serde/src/lib.rs
+++ exporter/serde/src/lib.rs
@@ -1,25 +1,4 @@
-use std::error::Error;
-
-use typst::{diag::SourceResult, World};
-
-/// Convert the given error to a vector of source errors.
-fn map_err<E: Error>(world: &dyn World, e: E) -> Box<Vec<typst::diag::SourceError>> {
-    Box::new(vec![typst::diag::SourceError::new(
-        typst::syntax::Span::new(world.main().id(), 0),
-        e.to_string(),
-    )])
-}
-
-/// export document to file system
-fn write_to_path<C: AsRef<[u8]>>(
-    world: &dyn World,
-    path: Option<std::path::PathBuf>,
-    content: C,
-) -> SourceResult<()> {
-    path.map_or(Ok(()), |path| {
-        std::fs::write(path, content).map_err(|e| map_err(world, e))
-    })
-}
+pub(crate) use typst_ts_core::exporter_utils::*;
 
 pub(crate) mod macros;
 
