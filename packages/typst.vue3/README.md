c3e5e55acf77f530885ec62f637a4c0711f2e778
Myriad-Dreamin <camiyoru@gmail.com>
2023-05-06 19:12:15 +0800

    dev(pkg::vue3): init

---
+++ packages/typst.vue3/README.md
@@ -0,0 +1,3 @@
+# Typst.vue3
+
+This project was generated with [create-vite](https://github.com/vitejs/vite/tree/main/packages/create-vite) version 4.3.1.
---
+++ packages/typst.vue3/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <!-- <img src="/vite.svg" class="logo" alt="Vite logo" /> -->
+      <img
+        src="https://raw.githubusercontent.com/vitejs/vite/c63ba3fa08a64d75bfffa6885dc4c44875b9c5ba/docs/public/logo.svg"
+        class="logo"
+        alt="Vite logo"
+      />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
+      <img
+        src="https://raw.githubusercontent.com/vuejs/core/862edfd91a2c2f6b75f943cb1a9682c4be5d7fa8/packages/sfc-playground/public/logo.svg"
+        class="logo vue"
+        alt="Vue logo"
+      />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
