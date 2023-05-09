31d9d04f7b2684266577619e8c0c36c96e698d97
Myriad-Dreamin <camiyoru@gmail.com>
2023-05-09 14:42:23 +0800

    dev(compiler): generalize ReadFontLoader

--- compiler/src/font/reader/mod.rs
+++ compiler/src/font/reader/mod.rs
@@ -1,16 +1,15 @@
-use std::io::Read;
 use typst::font::Font;
 use typst_ts_core::FontLoader;
 
 #[cfg(feature = "system")]
 pub mod system;
 
-pub struct ReadFontLoader {
-    pub read: Box<dyn std::io::Read>,
+pub struct ReadFontLoader<R> {
+    pub read: R,
     pub index: u32,
 }
 
-impl FontLoader for ReadFontLoader {
+impl<R: std::io::Read + Sized> FontLoader for ReadFontLoader<R> {
     fn load(&mut self) -> Option<Font> {
         let mut buf = vec![];
         self.read.read_to_end(&mut buf).ok()?;
