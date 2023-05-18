0ced03d387e383b6837076524d69d0148507b597
Myriad-Dreamin <camiyoru@gmail.com>
2023-05-18 13:10:48 +0800

    dev(core): slot inner using QueryRef

--- core/src/font/slot.rs
+++ core/src/font/slot.rs
@@ -1,49 +1,37 @@
-use std::sync::{Arc, Mutex};
-
-use once_cell::sync::OnceCell;
 use typst::font::Font;
 
-use crate::FontLoader;
+use crate::{FontLoader, QueryRef};
+
+type FontSlotInner = QueryRef<Option<Font>, (), Box<dyn FontLoader>>;
 
 /// Lazy Font Reference, load as needed.
-pub struct FontSlot(
-    /// reference to the loaded font
-    OnceCell<Option<Font>>,
-    /// reference to the font loader
-    Arc<Mutex<Option<Box<dyn FontLoader>>>>,
-);
+pub struct FontSlot(FontSlotInner);
 
 impl FontSlot {
     pub fn with_value(f: Option<Font>) -> Self {
-        Self(OnceCell::with_value(f), Arc::new(Mutex::new(None)))
+        Self(FontSlotInner::with_value(f))
     }
 
     pub fn new(f: Box<dyn FontLoader>) -> Self {
-        Self(OnceCell::new(), Arc::new(Mutex::new(Some(f))))
+        Self(FontSlotInner::with_context(f))
     }
 
     pub fn new_boxed<F: FontLoader + 'static>(f: F) -> Self {
         Self::new(Box::new(f))
     }
 
-    fn load(&self) -> Option<Font> {
-        let mut init_fn = self.1.lock().unwrap();
-        init_fn
-            .take()
-            .expect("the initialization fn is already poisoned")
-            .load()
-    }
-
-    /// Gets the reference to the font load result.
+    /// Gets the reference to the font load result (possible uninitialized).
     ///
     /// Returns `None` if the cell is empty, or being initialized. This
     /// method never blocks.
-    pub fn get_uninitialized(&self) -> Option<&Option<Font>> {
-        self.0.get()
+    pub fn get_uninitialized(&self) -> Option<Option<Font>> {
+        let query_res = self.0.get_uninitialized().clone();
+        query_res.map(|res| unsafe { res.unwrap_unchecked() })
     }
 
     /// Gets or make the font load result.
     pub fn get_or_init(&self) -> Option<Font> {
-        self.0.get_or_init(|| self.load()).clone()
+        let res = self.0.compute_with_context(|mut c| Ok(c.load()));
+        { unsafe { res.unwrap_unchecked() } }.clone()
     }
 }
