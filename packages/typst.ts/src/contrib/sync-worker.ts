f21675a59e5049fe217d118002075c4db823945b
Myriad-Dreamin <camiyoru@gmail.com>
2023-05-21 07:51:05 +0800

    dev(pkg::core): fix compile error

--- packages/typst.ts/src/contrib/sync-worker.ts
+++ packages/typst.ts/src/contrib/sync-worker.ts
@@ -2,14 +2,16 @@
 //!   See https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/fetch_event
 //! It is quite important to note that the service worker can only intercept requests that has same origin.
 //! For example, if you have registered this worker with url:
-//!   register('typst.ts/article/event-loop-worker.js')
+//!   register('typst.ts/article/sync-worker.js')
 //! This worker will be able to intercept requests that has same origin as the above url:
 //!   typst.ts/article/...
 //! But it will not be able to intercept requests that has different origin:
 //!   typst.ts/book/...
 //!   typst.ts/...
 //! To intercept requests that has different origin, you need to register the worker with url:
-//!   typst.ts/event-loop-worker.js
+//!   register('typst.ts/sync-worker.js')
 //! or all urls that you want to intercept:
-//!   register('typst.ts/article/event-loop-worker.js')
-//!   register('typst.ts/book/event-loop-worker.js')
+//!   register('typst.ts/article/sync-worker.js')
+//!   register('typst.ts/book/sync-worker.js')
+
+export function registerSyncWorker(_workerArgs: unknown) {}
