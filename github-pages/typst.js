ae634f70b3a38503bc7f17e7fcfd1a2def37a0d6
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-06-06 14:59:08 +0800

    dev(gh): try using partial fonts (#114)

--- github-pages/typst.js
+++ github-pages/typst.js
@@ -5,13 +5,15 @@
       .init({
         beforeBuild: [
           window.TypstRenderModule.preloadRemoteFonts([
-            '/typst.ts/assets/fonts/LinLibertine_R.ttf',
-            '/typst.ts/assets/fonts/LinLibertine_RB.ttf',
-            '/typst.ts/assets/fonts/LinLibertine_RBI.ttf',
-            '/typst.ts/assets/fonts/LinLibertine_RI.ttf',
+            // use glyphs.json
+            // '/typst.ts/assets/fonts/LinLibertine_R.ttf',
+            // '/typst.ts/assets/fonts/LinLibertine_RB.ttf',
+            // '/typst.ts/assets/fonts/LinLibertine_RBI.ttf',
+            // '/typst.ts/assets/fonts/LinLibertine_RI.ttf',
+            // '/typst.ts/assets/fonts/NewCMMath-Regular.otf',
+            // '/typst.ts/assets/fonts/DejaVuSansMono.ttf',
+            // not used
             // '/typst.ts/assets/fonts/NewCMMath-Book.otf',
-            '/typst.ts/assets/fonts/NewCMMath-Regular.otf',
-            '/typst.ts/assets/fonts/DejaVuSansMono.ttf',
             // '/typst.ts/assets/fonts/DejaVuSansMono-Bold.ttf',
             // '/typst.ts/assets/fonts/DejaVuSansMono-BoldOblique.ttf',
             // '/typst.ts/assets/fonts/DejaVuSansMono-Oblique.ttf',
@@ -20,15 +22,30 @@
         getModule: () => '/typst.ts/renderer/typst_ts_renderer_bg.wasm',
       })
       .then(() => {
+        let artifactContent = undefined;
+
+        const getGlyphs = fetch('/typst.ts/docs/readme.glyphs.json')
+          .then(response => response.text())
+          .then(content => JSON.parse(content))
+          .catch(e => {
+            console.log(e);
+            return undefined;
+          });
+
         fetch('/typst.ts/docs/readme.artifact.json')
           .then(response => response.arrayBuffer())
           .then(buffer => new Uint8Array(buffer))
           .then(buffer => {
+            artifactContent = buffer;
+          })
+          .then(() => getGlyphs)
+          .then(glyphPack => plugin.loadGlyphPack(glyphPack))
+          .then(() => {
             const appContainer = document.getElementById('typst-app');
 
             plugin
               .render({
-                artifactContent: buffer,
+                artifactContent,
                 container: appContainer,
                 backgroundColor: '#343541',
                 pixelPerPt: 4.5,
