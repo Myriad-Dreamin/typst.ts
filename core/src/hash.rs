a8c2838a1ef0a2dedb686ec77a7ab13cbfde0ae3
Myriad-Dreamin <camiyoru@gmail.com>
2023-06-27 18:04:48 +0800

    dev(core): replace typst_affinite_hash by typst::util::hash128

--- core/src/hash.rs
+++ core/src/hash.rs
@@ -1,13 +1,11 @@
-use siphasher::sip128::{Hasher128, SipHasher13};
-
 /// This function maintain hash function corresponding to Typst
 /// Typst changed the hash function from `siphasher::sip128::SipHasher` to
 ///   `siphasher::sip128::SipHasher13` since commit
 ///   <https://github.com/typst/typst/commit/d0afba959d18d1c2c646b99e6ddd864b1a91deb2>
 /// Commit log:
 /// This seems to significantly improves performance. Inspired by rust-lang/rust#107925
+///
+/// Update: Use Typst's new util function `typst::util::hash128`
 pub fn typst_affinite_hash<T: std::hash::Hash>(t: &T) -> u128 {
-    let mut s = SipHasher13::new();
-    t.hash(&mut s);
-    s.finish128().as_u128()
+    typst::util::hash128(t)
 }
