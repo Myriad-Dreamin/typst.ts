65c0165f75dc3e69e85bb4ebe74c5966e5f34d5f
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-06-27 18:44:53 +0800

    dev(cli): improve readability (#166)

--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -12,3 +12,17 @@ pub fn async_continue<F: std::future::Future<Output = ()>>(f: F) -> ! {
         unreachable!("The async command must exit the process.");
     }
 }
+
+/// Exit status code used for successful compilation and help output.
+pub const EXIT_SUCCESS: i32 = 0;
+
+/// Exit status code used for compilation failures and invalid flags.
+pub const EXIT_FAILURE: i32 = 1;
+
+pub fn logical_exit(is_success: bool) -> ! {
+    std::process::exit(if is_success {
+        EXIT_SUCCESS
+    } else {
+        EXIT_FAILURE
+    })
+}
