0b685b7ae0d8f683b8c225895fd54f2f3089e60f
Myriad-Dreamin <camiyoru@gmail.com>
2023-07-19 06:11:56 +0800

    dev(core): rearrange top library file

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,35 +1,34 @@
-pub mod annotation;
-
-pub mod artifact;
-pub use artifact::{core::ArtifactMeta, Artifact};
-
-pub mod artifact_ir;
-pub mod vector;
-
-pub mod cache;
-
-pub mod config;
-
-pub mod content;
-pub use content::TextContent;
-
+// Core type system/concepts of typst-ts.
+pub(crate) mod concepts;
+pub use concepts::*;
 pub mod error;
 pub use error::{ErrKind, Error};
 
+pub(crate) mod hash;
+pub use hash::typst_affinite_hash;
+pub mod path;
+
+// Core data structures of typst-ts.
+pub mod annotation;
+pub mod content;
+pub use content::TextContent;
+// todo: move me to compiler
+pub mod cache;
+pub mod config;
+
+// Core mechanism of typst-ts.
 pub(crate) mod exporter;
 pub use exporter::{builtins as exporter_builtins, utils as exporter_utils};
 pub use exporter::{mark_transformer_lambda, Exporter, Transformer};
-
 pub mod font;
 pub use font::{FontLoader, FontResolver, FontSlot};
 pub mod package;
 
-pub(crate) mod hash;
-pub mod path;
-pub use hash::typst_affinite_hash;
-
-pub(crate) mod concepts;
-pub use concepts::*;
+// Intermediate representation of typst-ts.
+pub mod artifact;
+pub use artifact::{core::ArtifactMeta, Artifact};
+pub mod artifact_ir;
+pub mod vector;
 
 pub mod build_info {
     /// The version of the typst-ts-core crate.
