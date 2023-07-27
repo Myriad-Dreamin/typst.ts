e80725b47cd0ac4be2f2138862c090b9b470ccef
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-07-27 10:58:44 +0800

    dev(server::dev): update github-page mapping (#260)

--- server/dev/src/http.rs
+++ server/dev/src/http.rs
@@ -16,7 +16,19 @@ pub async fn run_http(args: RunHttpArgs) {
     let corpora = warp::path("corpus").and(warp::fs::dir(args.corpus));
     let assets = warp::path("assets").and(warp::fs::dir("assets"));
     let core = warp::path("core").and(warp::fs::dir("packages/typst.ts"));
-    let gh_pages = warp::path("typst.ts").and(warp::fs::dir("github-pages"));
+
+    // map these files to the root of the github-pages server
+    let gh_pages = warp::path("typst.ts").and({
+        let renderer = warp::path("renderer").and(warp::fs::dir("packages/renderer/pkg"));
+        let compiler = warp::path("compiler").and(warp::fs::dir("packages/compiler/pkg"));
+        let typst_main =
+            warp::path("typst-main.js").and(warp::fs::file("packages/typst.ts/dist/main.js"));
+
+        renderer
+            .or(compiler)
+            .or(typst_main)
+            .or(warp::fs::dir("github-pages"))
+    });
 
     let cors = warp::cors().allow_methods(&[Method::GET, Method::POST, Method::DELETE]);
 
