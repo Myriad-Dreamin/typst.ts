81934fcb0456d239bc10a4069578db65dd61fa28
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-08-01 16:05:20 +0800

    dev(pkg::renderer): implement get_source_loc (#270)

--- packages/renderer/src/lib.rs
+++ packages/renderer/src/lib.rs
@@ -5,6 +5,7 @@ use typst_ts_core::error::prelude::*;
 use typst_ts_core::font::FontResolverImpl;
 use typst_ts_core::vector::geom::Axes;
 use typst_ts_core::vector::geom::Scalar;
+use typst_ts_svg_exporter::flat_ir::SourceMappingNode;
 use typst_ts_svg_exporter::IncrSvgDocClient;
 use typst_ts_svg_exporter::{DefaultExportFeature, LayoutElem, SvgExporter};
 use wasm_bindgen::prelude::*;
@@ -53,6 +54,12 @@ pub struct TypstRenderer {
 #[wasm_bindgen]
 pub struct SvgSession {
     client: IncrSvgDocClient,
+}
+
+fn access_slice<'a, T>(v: &'a [T], idx: usize, kind: &'static str, pos: usize) -> ZResult<&'a T> {
+    v.get(idx).ok_or_else(
+        || error_once!("out of bound access", pos: pos, kind: kind, idx: idx, actual: v.len()),
+    )
 }
 
 #[wasm_bindgen]
@@ -111,6 +118,69 @@ impl SvgSession {
             lo: Axes::new(Scalar(rect_lo_x), Scalar(rect_lo_y)),
             hi: Axes::new(Scalar(rect_hi_x), Scalar(rect_hi_y)),
         })
+    }
+
+    pub fn get_source_loc(&self, path: &[u32]) -> ZResult<Option<String>> {
+        const SOURCE_MAPPING_TYPE_TEXT: u32 = 0;
+        const SOURCE_MAPPING_TYPE_GROUP: u32 = 1;
+        const SOURCE_MAPPING_TYPE_IMAGE: u32 = 2;
+        const SOURCE_MAPPING_TYPE_SHAPE: u32 = 3;
+        const SOURCE_MAPPING_TYPE_PAGE: u32 = 4;
+
+        if self.client.page_source_mappping.is_empty() {
+            return Ok(None);
+        }
+
+        let mut index_item: Option<&SourceMappingNode> = None;
+
+        let source_mapping = self.client.source_mapping_data.as_slice();
+        let page_sources = self.client.page_source_mappping[0].as_slice();
+
+        for (chunk_idx, v) in path.chunks_exact(2).enumerate() {
+            let (ty, idx) = (v[0], v[1] as usize);
+
+            let this_item = match index_item {
+                Some(SourceMappingNode::Group(q)) => {
+                    let idx = *access_slice(q, idx, "group_index", chunk_idx)? as usize;
+                    access_slice(source_mapping, idx, "source_mapping", chunk_idx)?
+                }
+                Some(_) => {
+                    return Err(
+                        error_once!("cannot index", pos: chunk_idx, indexing: format!("{:?}", index_item)),
+                    )
+                }
+                None => access_slice(page_sources, idx, "page_sources", chunk_idx)?,
+            };
+
+            match (ty, this_item) {
+                (SOURCE_MAPPING_TYPE_PAGE, SourceMappingNode::Page(page_index)) => {
+                    index_item = Some(access_slice(
+                        source_mapping,
+                        *page_index as usize,
+                        "source_mapping",
+                        chunk_idx,
+                    )?);
+                }
+                (SOURCE_MAPPING_TYPE_GROUP, SourceMappingNode::Group(_)) => {
+                    index_item = Some(this_item);
+                }
+                (SOURCE_MAPPING_TYPE_TEXT, SourceMappingNode::Text(n))
+                | (SOURCE_MAPPING_TYPE_IMAGE, SourceMappingNode::Image(n))
+                | (SOURCE_MAPPING_TYPE_SHAPE, SourceMappingNode::Shape(n)) => {
+                    return Ok(Some(format!("{n:x}")));
+                }
+                _ => {
+                    return Err(error_once!("invalid/mismatch node type",
+                        pos: chunk_idx, ty: ty,
+                        actual: format!("{:?}", this_item),
+                        parent: format!("{:?}", index_item),
+                        child_idx_in_parent: idx,
+                    ))
+                }
+            }
+        }
+
+        Ok(None)
     }
 }
 
