f4f5cba9c6f6cdd4eac5f9a228f95bdca23c6041
Myriad-Dreamin <camiyoru@gmail.com>
2023-08-18 07:23:49 +0800

    dev(core): pub use typst's Abs

--- core/src/concepts/typst.rs
+++ core/src/concepts/typst.rs
@@ -3,3 +3,5 @@ pub type Bytes = typst::eval::Bytes;
 /// Although this is not good to expose this, we make an alias here to let it as
 /// a part of typst-ts.
 pub use typst::syntax::FileId as TypstFileId;
+
+pub use typst::geom::Abs as TypstAbs;
