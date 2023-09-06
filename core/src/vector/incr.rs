0287abd3b53135985b2796cfb843eb1dea217ac5
Myriad-Dreamin <camiyoru@gmail.com>
2023-09-06 22:48:32 +0800

    dev(core): move incr doc server

--- core/src/vector/incr.rs
+++ core/src/vector/incr.rs
@@ -1,9 +1,152 @@
-use crate::{error::prelude::*, TakeAs};
+use std::sync::Arc;
+
+use typst::doc::Document;
+
+use crate::{
+    error::prelude::*,
+    vector::{
+        flat_ir::{flatten_glyphs, FontPack, GlyphPack, ItemPack, LayoutRegion},
+        LowerBuilder,
+    },
+    TakeAs,
+};
 
 use super::flat_ir::{
-    FlatModule, LayoutRegionNode, LayoutSourceMapping, ModuleMetadata, MultiSvgDocument, Page,
-    SourceMappingNode,
+    FlatModule, IncrModuleBuilder, LayoutRegionNode, LayoutSourceMapping, ModuleMetadata,
+    MultiSvgDocument, Page, SourceMappingNode, SvgDocument,
 };
+
+/// maintains the data of the incremental rendering at server side
+#[derive(Default)]
+pub struct IncrDocServer {
+    /// Whether to attach debug info to the output.
+    should_attach_debug_info: bool,
+
+    /// Expected exact state of the current Compiler.
+    /// Initially it is None meaning no completed compilation.
+    doc_view: Option<SvgDocument>,
+
+    /// Maintaining document build status
+    module_builder: IncrModuleBuilder,
+
+    /// Optional page source mapping references.
+    page_source_mapping: Vec<SourceMappingNode>,
+}
+
+impl IncrDocServer {
+    pub fn set_should_attach_debug_info(&mut self, should_attach_debug_info: bool) {
+        self.module_builder.should_attach_debug_info = should_attach_debug_info;
+        self.should_attach_debug_info = should_attach_debug_info;
+    }
+
+    /// Pack the delta into a binary blob.
+    pub fn pack_delta(&mut self, output: Arc<Document>) -> Vec<u8> {
+        self.module_builder.reset();
+        self.page_source_mapping.clear();
+
+        let instant: std::time::Instant = std::time::Instant::now();
+
+        self.module_builder.increment_lifetime();
+
+        // it is important to call gc before building pages
+        let gc_items = self.module_builder.gc(5 * 2);
+
+        let mut lower_builder = LowerBuilder::new(&output);
+        let builder = &mut self.module_builder;
+        let pages = output
+            .pages
+            .iter()
+            .map(|p| {
+                let abs_ref = builder.build(lower_builder.lower(p));
+                if self.should_attach_debug_info {
+                    self.page_source_mapping.push(SourceMappingNode::Page(
+                        (builder.source_mapping.len() - 1) as u64,
+                    ));
+                }
+
+                Page {
+                    content: abs_ref,
+                    size: p.size().into(),
+                }
+            })
+            .collect::<Vec<_>>();
+        let delta = builder.finalize_delta();
+
+        // max, min lifetime current, gc_items
+        #[cfg(feature = "debug_gc")]
+        println!(
+            "gc: max: {}, min: {}, curr: {}, {}",
+            self.module_builder
+                .items
+                .values()
+                .map(|i| i.0)
+                .max()
+                .unwrap_or(0xffffffff),
+            self.module_builder
+                .items
+                .values()
+                .map(|i| i.0)
+                .min()
+                .unwrap_or(0),
+            self.module_builder.lifetime,
+            gc_items.len()
+        );
+
+        let fonts = FontPack {
+            items: delta.fonts,
+            incremental_base: 0, // todo: correct incremental_base
+        };
+
+        let glyphs = GlyphPack {
+            items: flatten_glyphs(delta.glyphs),
+            incremental_base: 0, // todo: correct incremental_base
+        };
+
+        let pages = LayoutRegionNode::new_pages(pages.clone());
+        let pages = Arc::new(LayoutRegion::new_single(pages));
+
+        let delta = FlatModule::new(vec![
+            ModuleMetadata::SourceMappingData(delta.source_mapping),
+            ModuleMetadata::PageSourceMapping(Arc::new(LayoutSourceMapping::new_single(
+                self.page_source_mapping.clone(),
+            ))),
+            ModuleMetadata::GarbageCollection(gc_items),
+            ModuleMetadata::Font(Arc::new(fonts)),
+            ModuleMetadata::Glyph(Arc::new(glyphs)),
+            ModuleMetadata::Item(ItemPack(delta.items.clone().into_iter().collect())),
+            ModuleMetadata::Layout(pages),
+        ])
+        .to_bytes();
+
+        log::info!("svg render time (incremental bin): {:?}", instant.elapsed());
+        [b"diff-v1,", delta.as_slice()].concat()
+    }
+
+    /// Pack the current entirely into a binary blob.
+    pub fn pack_current(&mut self) -> Option<Vec<u8>> {
+        let doc = self.doc_view.as_ref()?;
+
+        let (fonts, glyphs) = self.module_builder.glyphs.finalize();
+        let glyphs = flatten_glyphs(glyphs);
+
+        let pages = LayoutRegionNode::new_pages(doc.pages.clone());
+        let pages = Arc::new(LayoutRegion::new_single(pages));
+
+        let delta = FlatModule::new(vec![
+            ModuleMetadata::SourceMappingData(self.module_builder.source_mapping.clone()),
+            ModuleMetadata::PageSourceMapping(Arc::new(LayoutSourceMapping::new_single(
+                self.page_source_mapping.clone(),
+            ))),
+            // todo: correct incremental_base
+            ModuleMetadata::Font(Arc::new(fonts.into())),
+            ModuleMetadata::Glyph(Arc::new(glyphs.into())),
+            ModuleMetadata::Item(ItemPack(doc.module.items.clone().into_iter().collect())),
+            ModuleMetadata::Layout(pages),
+        ])
+        .to_bytes();
+        Some([b"new,", delta.as_slice()].concat())
+    }
+}
 
 /// maintains the data of the incremental rendering at client side
 #[derive(Default)]
