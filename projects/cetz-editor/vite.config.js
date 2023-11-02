6a98671d94b7dad0b14cb48d97d2beff0ac692bc
Myriad-Dreamin <camiyoru@gmail.com>
2023-11-02 21:26:24 +0800

    feat: add cetz-editor

---
+++ projects/cetz-editor/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  plugins: [viteSingleFile()],
+  build: {
+    outDir: 'out',
+    minify: false,
+  },
+});
