1be43b1ff2a1b63197264021fec1202c1af2fb52
Myriad-Dreamin <camiyoru@gmail.com>
2023-11-04 17:40:38 +0800

    feat(pkg::parser): init

---
+++ packages/parser/src/lib.rs
@@ -0,0 +1,75 @@
+use std::{path::Path, sync::Arc};
+
+use js_sys::Uint32Array;
+use typst::syntax::{FileId, VirtualPath};
+use typst_ts_compiler::parser::{
+    get_semantic_tokens_full, get_semantic_tokens_legend, OffsetEncoding, SemanticToken,
+};
+use typst_ts_core::error::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct TypstParserBuilder {}
+
+#[wasm_bindgen]
+impl TypstParserBuilder {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> ZResult<TypstParserBuilder> {
+        console_error_panic_hook::set_once();
+        Ok(Self {})
+    }
+
+    pub async fn build(self) -> Result<TypstParser, JsValue> {
+        Ok(TypstParser {})
+    }
+}
+
+#[wasm_bindgen]
+pub struct TypstParser {}
+
+impl TypstParser {
+    pub fn get_semantic_tokens_inner(
+        &self,
+        src: String,
+        offset_encoding: OffsetEncoding,
+    ) -> Arc<Vec<SemanticToken>> {
+        let src = typst::syntax::Source::new(
+            FileId::new(None, VirtualPath::new(Path::new("/main.typ"))),
+            src,
+        );
+
+        Arc::new(get_semantic_tokens_full(&src, offset_encoding))
+    }
+}
+
+#[wasm_bindgen]
+impl TypstParser {
+    pub fn get_semantic_token_legend(&self) -> Result<JsValue, JsValue> {
+        let legend = get_semantic_tokens_legend();
+        serde_wasm_bindgen::to_value(&legend).map_err(|e| format!("{e:?}").into())
+    }
+
+    pub fn get_semantic_tokens_by_string(
+        &self,
+        src: String,
+        offset_encoding: String,
+    ) -> ZResult<Uint32Array> {
+        let tokens = self.get_semantic_tokens_inner(src, match offset_encoding.as_str() {
+            "utf-16" => OffsetEncoding::Utf16,
+           "utf-8" => OffsetEncoding::Utf8,
+             _ => {
+                 return Err(error_once!("Unsupported offset encoding", offset_encoding: offset_encoding));
+             }
+         });
+        let mut result = Vec::new();
+        for token in tokens.iter() {
+            result.push(token.delta_line);
+            result.push(token.delta_start_character);
+            result.push(token.length);
+            result.push(token.token_type);
+            result.push(token.token_modifiers);
+        }
+
+        Ok(Uint32Array::from(&result[..]))
+    }
+}
