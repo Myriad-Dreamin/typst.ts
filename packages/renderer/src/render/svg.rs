889c64b5ba4f71461d5b8647df767a23bd1a26d8
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2023-11-29 10:28:59 +0800

    feat: add getCustomV1 api (#427)

--- packages/renderer/src/render/svg.rs
+++ packages/renderer/src/render/svg.rs
@@ -1,9 +1,9 @@
 use crate::RenderSession;
 use crate::TypstRenderer;
+use js_sys::Uint8Array;
 use typst_ts_core::error::prelude::*;
 use typst_ts_core::vector::geom::Axes;
 use typst_ts_core::vector::geom::Scalar;
-use typst_ts_svg_exporter::flat_ir::LayoutRegionNode;
 use typst_ts_svg_exporter::SvgDataSelection;
 use typst_ts_svg_exporter::{DefaultExportFeature, SvgExporter};
 use wasm_bindgen::prelude::*;
@@ -67,63 +67,78 @@ impl TypstRenderer {
         Ok(svg)
     }
 
-    pub fn render_svg(&self, session: &RenderSession, root: web_sys::HtmlElement) -> ZResult<()> {
+    pub fn get_customs(&self, session: &RenderSession) -> Option<js_sys::Array> {
+        let client = session.client.lock().unwrap();
+        let layout = client.layout.clone();
+
+        layout.map(|layout| {
+            let view = layout.pages(client.module()).unwrap();
+            view.customs()
+                .map(|(k, v)| {
+                    [JsValue::from_str(k), Uint8Array::from(v.as_ref()).into()]
+                        .into_iter()
+                        .collect::<js_sys::Array>()
+                })
+                .collect::<js_sys::Array>()
+        })
+    }
+
+    pub fn render_svg(&self, session: &RenderSession, root: web_sys::HtmlElement) -> ZResult<bool> {
         type UsingExporter = SvgExporter<DefaultExportFeature>;
         // todo: leaking abstraction
-        let client = session.client.lock().unwrap();
+        let mut client = session.client.lock().unwrap();
         let layouts = client.doc.layouts[0].by_scalar().unwrap();
-        let layout = layouts.first().unwrap();
+        let mut layout = layouts.first().unwrap();
 
         // base scale = 2
         let base_cw = root.client_width() as f32;
 
-        let render = |layout: &(Scalar, LayoutRegionNode)| {
-            let applying = format!("{}px", layout.0 .0);
+        const EPS: f32 = 1e-2;
 
-            let applied = root.get_attribute("data-applied-width");
-            if applied.is_some() && applied.unwrap() == applying {
-                // console_log!("already applied {}", applying);
-                return Ok(());
+        if layout.0 .0 >= base_cw + EPS {
+            let layout_alt = layouts.last().unwrap();
+
+            if layout_alt.0 .0 + EPS > base_cw {
+                layout = layout_alt;
+            } else {
+                for layout_alt in layouts {
+                    if layout_alt.0 .0 < base_cw + EPS {
+                        layout = layout_alt;
+                        break;
+                    }
+                }
             }
+        }
 
-            let view = layout.1.pages(client.module()).unwrap();
+        let layout = layout.clone();
+        client.set_layout(layout.1.clone());
 
-            let svg = UsingExporter::render_flat_svg(view.module(), view.pages(), None);
-            root.set_inner_html(&svg);
-            let window = web_sys::window().unwrap();
-            if let Ok(proc) = js_sys::Reflect::get(&window, &JsValue::from_str("typstProcessSvg")) {
-                proc.dyn_ref::<js_sys::Function>()
-                    .unwrap()
-                    .call1(&JsValue::NULL, &root.first_element_child().unwrap())
-                    .unwrap();
-            }
+        // caching updation
+        let applying = format!("{}px", layout.0 .0);
+        let applied = root.get_attribute("data-applied-width");
+        if applied.is_some() && applied.unwrap() == applying {
+            // console_log!("already applied {}", applying);
+            return Ok(true);
+        }
 
-            root.set_attribute("data-applied-width", &applying).unwrap();
-            // console_log!("applied {}", applying);
-
-            Ok(())
-        };
+        let view = layout.1.pages(client.module()).unwrap();
+        let svg = UsingExporter::render_flat_svg(view.module(), view.pages(), None);
+        drop(client);
 
         // console_log!("base_cw {}", base_cw);
 
-        const EPS: f32 = 1e-2;
-
-        if layout.0 .0 < base_cw + EPS {
-            return render(layout);
+        root.set_inner_html(&svg);
+        let window = web_sys::window().unwrap();
+        if let Ok(proc) = js_sys::Reflect::get(&window, &JsValue::from_str("typstProcessSvg")) {
+            proc.dyn_ref::<js_sys::Function>()
+                .unwrap()
+                .call1(&JsValue::NULL, &root.first_element_child().unwrap())
+                .unwrap();
         }
 
-        let layout = layouts.last().unwrap();
+        root.set_attribute("data-applied-width", &applying).unwrap();
+        // console_log!("applied {}", applying);
 
-        if layout.0 .0 + EPS > base_cw {
-            return render(layout);
-        }
-
-        for layout in layouts {
-            if layout.0 .0 < base_cw + EPS {
-                return render(layout);
-            }
-        }
-
-        Ok(())
+        Ok(false)
     }
 }
