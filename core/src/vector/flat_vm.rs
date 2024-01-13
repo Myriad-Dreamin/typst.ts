85db02d5a40e4593402c3741744ab9248fac4fa3
Myriad-Dreamin <camiyoru@gmail.com>
2024-01-13 16:16:49 +0800

    fix: reset diff group state

--- core/src/vector/flat_vm.rs
+++ core/src/vector/flat_vm.rs
@@ -294,7 +294,7 @@ where
         sz: &Option<Size>,
     ) {
         if let Some(sz) = sz {
-            state = state.with_size(*sz);
+            state = state.with_transform(Transform::identity()).with_size(*sz);
         }
 
         if let Some(ir::FlatSvgItem::Group(prev_group, _)) = prev_item_ {
