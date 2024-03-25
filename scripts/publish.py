9b473be083f13162af4c3cc5f0f0d79d80061229
Myriad-Dreamin <camiyoru@gmail.com>
2024-03-25 10:29:58 +0800

    dev: add publish script

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,3 +1,11 @@
 
 
+import subprocess
 
+pub = ["cargo", "publish", "-p"]
+feats = ["--features", "no-content-hint"]
+subprocess.run([*pub, "reflexo"])
+subprocess.run([*pub, "reflexo-vec2canvas"])
+subprocess.run([*pub, "typst-ts-core", *feats])
+subprocess.run([*pub, "typst-ts-svg-exporter", *feats])
+subprocess.run([*pub, "typst-ts-compiler", *feats])
