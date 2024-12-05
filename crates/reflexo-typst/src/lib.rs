562934cb172675d7a8dab9b63c76aeade9c595ab
Myriad-Dreamin <camiyoru@gmail.com>
2024-12-05 12:05:37 +0800

    feat: add html target compilation

--- crates/reflexo-typst/src/lib.rs
+++ crates/reflexo-typst/src/lib.rs
@@ -399,12 +399,12 @@ pub trait Compiler {
     }
 
     /// Compile once from scratch.
-    fn pure_compile(
+    fn pure_compile_<D: ::typst::Document>(
         &mut self,
         world: &Self::W,
         _env: &mut CompileEnv,
-    ) -> SourceResult<Warned<Arc<TypstDocument>>> {
-        let res = ::typst::compile(world);
+    ) -> SourceResult<Warned<Arc<D>>> {
+        let res = ::typst::compile::<D>(world);
         // compile document
         // res.output.map(Arc::new)
         match res.output {
@@ -425,6 +425,15 @@ pub trait Compiler {
         }
     }
 
+    /// Compile once from scratch.
+    fn pure_compile(
+        &mut self,
+        world: &Self::W,
+        env: &mut CompileEnv,
+    ) -> SourceResult<Warned<Arc<TypstDocument>>> {
+        self.pure_compile_(world, env)
+    }
+
     /// With **the compilation state**, query the matches for the selector.
     fn pure_query(
         &mut self,
@@ -442,6 +451,15 @@ pub trait Compiler {
         env: &mut CompileEnv,
     ) -> SourceResult<Warned<Arc<TypstDocument>>> {
         self.pure_compile(world, env)
+    }
+
+    /// Compile to html once from scratch.
+    fn compile_html(
+        &mut self,
+        world: &Self::W,
+        env: &mut CompileEnv,
+    ) -> SourceResult<Warned<Arc<::typst::html::HtmlDocument>>> {
+        self.pure_compile_(world, env)
     }
 
     /// With **the compilation state**, query the matches for the selector.
