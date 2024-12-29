6a4e2ba1aee2b8f31929761db7a77283e2b10738
Myriad-Dreamin <camiyoru@gmail.com>
2024-12-29 10:42:00 +0800

    fix: use `reflexo::time::now` to get time in browser

--- crates/reflexo/src/time.rs
+++ crates/reflexo/src/time.rs
@@ -2,11 +2,13 @@ pub use instant::Duration;
 pub use instant::Instant;
 pub use instant::SystemTime as Time;
 
+/// Returns the UTC+0 time.
 #[cfg(any(feature = "system", feature = "web"))]
 pub fn now() -> Time {
     Time::now()
 }
 
+/// Returns a dummy time on environments that do not support time.
 #[cfg(not(any(feature = "system", feature = "web")))]
 pub fn now() -> Time {
     Time::UNIX_EPOCH
