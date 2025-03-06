22fc2fa5c148a63c5160f32ad902deb541ba9043
sjfhsjfh <sjfhsjfh@qq.com>
2025-03-07 01:00:35 +0800

    test: add cli parts

--- projects/vite-plugin-typst/examples/typst-cli-mixin-parts/vite.config.mjs
+++ projects/vite-plugin-typst/examples/typst-cli-mixin-parts/vite.config.mjs
@@ -4,11 +4,10 @@ import { TypstPlugin, checkExecResult } from '@myriaddreamin/vite-plugin-typst';
 export default defineConfig({
   plugins: [
     TypstPlugin({
-      compiler: 'typst-cli',
       onResolveParts: (input, project, ctx) => {
-        const res = checkExecResult(input, project.tryHtml(input), ctx);
+        const res = checkExecResult(input, project.compileHtml(input), ctx);
         return {
-          frontmatter: res && project.query(input, { selector: '<frontmatter>', field: 'value' })[0],
+          frontmatter: res && project.query(res, { selector: '<frontmatter>', field: 'value' })[0],
         };
       },
     }),
