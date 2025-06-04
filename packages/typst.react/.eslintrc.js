4cc221efe17787690ba51dbb9c08a495b10e12bc
Myriad-Dreamin <camiyoru@gmail.com>
2025-06-05 00:45:17 +0800

    fix(build): fix errors during publishing v0.6.1-rc1

--- packages/typst.react/.eslintrc.js
+++ packages/typst.react/.eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/no-unresolved': ['error', { ignore: ['\\.css(?:\\?.*)?$'] }],
   },
 };
