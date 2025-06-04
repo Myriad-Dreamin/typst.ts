84f7169db5fa805409361999dfc001332196529b
Myriad-Dreamin <camiyoru@gmail.com>
2025-06-05 03:12:00 +0800

    feat: update descriptions about rustdoc-typst-demo

--- projects/rustdoc-typst-demo/README.md
+++ projects/rustdoc-typst-demo/README.md
@@ -1,10 +1,17 @@
 # rustdoc-typst-demo
 
-This crate demonstrates an approach to including KaTeX in Rust docs. See
-[this crate on docs.rs](https://docs.rs/rustdoc-typst-demo) for a demo of rendered KaTeX and an explanation of how to
-add KaTeX to your own crate.
+This crate demonstrates an approach to including Typst syntax in Rust docs. See
+[this crate on docs.rs](https://docs.rs/rustdoc-typst-demo) for a demo of rendered Typst and an explanation of how to
+add Typst to your own crate.
 
 License: MIT/Apache-2.0
+
+## Todo List
+
+A online generator for `typst-header.html`:
+
+- [ ] Using specific typst version.
+- [ ] Using fonts from "Google Fonts" or other font providers.
 
 ## Acknowledgements
 
--- projects/rustdoc-typst-demo/src/lib.rs
+++ projects/rustdoc-typst-demo/src/lib.rs
@@ -3,17 +3,26 @@
 //!
 //! Docs with Typst can be generated locally and [on docs.rs](https://docs.rs/rustdoc-typst-demo).
 //!
+//! How it works
+//! ============
+//!
+//! It downloads compilers, renderers, and fonts from CDNs and uses them to
+//! render Typst code in the documentation. This will take a few seconds the
+//! first time you open the documentation, but resources will be cached in your
+//! browser, so subsequent visits will be much faster.
+//!
 //! Setup
 //! =====
 //!
-//! You'll only need one file: just grab `typst-header.html` from this project and put it into the
-//! root of your project.
+//! You'll only need one file: just grab `typst-header.html` from this project
+//! and put it into the root of your project.
 //!
 //! ## Rendering Locally
 //!
-//! This project can be documented locally with the following commands. Dependencies are
-//! documented separately because you probably don't want your dependencies' docs to use Typst.
-//! Also, dependencies would not build correctly because of relative paths.
+//! This project can be documented locally with the following commands.
+//! Dependencies are documented separately because you probably don't want your
+//! dependencies' docs to use Typst. Also, dependencies would not build
+//! correctly because of relative paths.
 //!
 //! ```bash
 //! cargo doc
@@ -29,17 +38,34 @@
 //! rustdoc-args = [ "--html-in-header", "typst-header.html" ]
 //! ```
 //!
+//! Typst Compatibility
+//! ===================
+//!
+//! To ensure that your Typst code will always render correctly, you should lock
+//! the version of Typst used in your project. Please edit the
+//! `typst-header.html` and replace the version numbers with the ones you want
+//! to use:
+//!
+//! ```
+//! @myriaddreamin/typst.ts@v0.6.1-rc1
+//! @myriaddreamin/typst-ts-web-compiler@v0.6.1-rc1
+//! @myriaddreamin/typst-ts-renderer@v0.6.1-rc1
+//! ```
+//!
+//! The `@myriaddreamin/typst.ts@v0.6.1-rc1` should compile documents using
+//! typst v0.13.1.
+//!
 //! How to Write Typst
 //! ==================
 //!
 //! Here is some inline markup `{$integral f(x) dif x$}`.
 //!
 //! And now for a fancy math expression:
-//! 
+//!
 //! $ f(x) = integral_(-oo)^oo hat(f)(xi) e^(2 pi i xi x) dif x $
-//! 
-//! For complex markup, you can use the `typ-render`, `typc-render`, or `typm-render` directive. This is a bit more
-//! robust:
+//!
+//! For complex markup, you can use the `typ-render`, `typc-render`, or
+//! `typm-render` directive. This is a bit more robust:
 //!
 //! ````md
 //! ```typm-render
@@ -80,3 +106,10 @@
 //!   )
 //! }
 //! ```
+//!
+//! ## Todo List
+//!
+//! A online generator for `typst-header.html`:
+//!
+//! - Using specific typst version.
+//! - Using fonts from "Google Fonts" or other font providers.
