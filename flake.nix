31b6ca7e88dc05ad3819d9d9feb980435a174e97
Raul Escobar <email@raulesco.bar>
2025-08-20 21:26:26 -0500

    devshell

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
+        buildInputs = with pkgs; [
+          openssl
+          sqlite
+          cargo-watch
+          sqlx-cli
+          stripe-cli
+          pulumi
+          pulumiPackages.pulumi-nodejs
+          nodejs_24
+          #bun
+          cargo-lambda
+          awscli2
+          concurrently
+          tinymist
+          pandoc
+          pnpm 
+        ];
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+            stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
+            RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+          };
+        }
+    );
+}
