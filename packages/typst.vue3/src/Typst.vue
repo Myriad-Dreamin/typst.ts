573bc57a7542ec7bbc0af7e6fadc1e82e855e735
bryarrow <220811010104@qhu.edu.cn>
2025-09-22 15:51:23 +0800

    fix: add a separate file to set init options to avoid repeatedly calling setXXXInitOptions

--- packages/typst.vue3/src/Typst.vue
+++ packages/typst.vue3/src/Typst.vue
@@ -5,22 +5,7 @@
 <script setup lang="ts">
 import { reactive, onMounted, watch } from 'vue';
 import { $typst } from '@myriaddreamin/typst.ts';
-
-// Prevents reinitialization of compiler and renderer options during HMR (Hot Module Replacement).
-// Use prepareUseOnce flag ensures initialization occurs only once to avoid duplicate calls to setXXXInitOptions.
-if (!$typst.prepareUseOnce) {
-  $typst.setCompilerInitOptions({
-    beforeBuild: [],
-    getModule: () =>
-      'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm',
-  });
-
-  $typst.setRendererInitOptions({
-    beforeBuild: [],
-    getModule: () =>
-      'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm',
-  });
-}
+import setTypst from './set-init-options-typst.ts';
 
 interface prop {
   content: string;
@@ -35,6 +20,7 @@ const props = withDefaults(defineProps<prop>(), {
 });
 
 onMounted(async () => {
+  setTypst();
   typst.compiled = await $typst.svg({ mainContent: props.content });
 });
 
