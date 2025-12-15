343130fb27d458ee5f7c4898bf3238a5ce523937
Tom <35542432+AquaBx@users.noreply.github.com>
2025-12-15 14:05:46 +0100

    Update Typst.svelte

--- packages/typst.svelte/src/lib/Typst.svelte
+++ packages/typst.svelte/src/lib/Typst.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import setTypst from "./set-init-options-typst.ts";
-	const { mainContent = "" }: { mainContent: string } = $props();
-	const typst = setTypst();
+  import setTypst from './set-init-options-typst.ts';
+  const { mainContent = '' }: { mainContent: string } = $props();
+  const typst = setTypst();
 </script>
 
-<div>
-	{#await typst.svg({ mainContent }) then compiled}
-		{@html compiled}
-	{/await}
-</div>
+{#await typst.svg({ mainContent }) then compiled}
+  {@html compiled}
+{/await}
