2928a8eab124595337f2478e5318556fdc75ac2e
Tom <35542432+AquaBx@users.noreply.github.com>
2025-12-15 13:42:38 +0100

    playwright test

--- packages/typst.svelte/playwright.config.ts
+++ packages/typst.svelte/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from 'playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'npm run dev', port: 5173 },
